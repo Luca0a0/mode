f2384f67c303a7f2cdadd3c8f314af79599f0dbf
“Luca <luca.angela@edu.unito.it>
2025-03-20 18:39:32 +0100

    update

--- minecraf/forge-1.20-46.0.14-mdk/src/main/java/net/Luca/firstMod/ExampleMod.java
+++ minecraf/forge-1.20-46.0.14-mdk/src/main/java/net/Luca/firstMod/ExampleMod.java
@@ -1,17 +1,6 @@
-package net.Luca.firstMod;
+package net.luca.firstMod;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -22,13 +11,10 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(firstMod.MOD_ID)
+@Mod(net.Luca.firstMod.firstMod.MOD_ID)
 public class firstMod
 {
     // Define mod id in a common place for everything to reference
